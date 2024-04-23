d398779d7fd17d8b7b9bea22dc420b17a9b45834
Joel Hendrix <jhendrix@microsoft.com>
2024-04-23 13:39:33 -0700

    fix import for test

--- packages/typespec-rust/test/codegen.test.ts
+++ packages/typespec-rust/test/codegen.test.ts
@@ -3,7 +3,7 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import * as rust from '../src/codemodel/codemodel.js';
+import * as rust from '../src/codemodel/index.js';
 import { generateCargoTomlFile } from '../src/codegen/cargotoml.js';
 import { strictEqual } from 'assert';
 import { describe, it } from 'vitest';
