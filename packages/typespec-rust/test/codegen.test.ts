34a13f1e49a7ff99e0cd2524ff3e0d1b5fe024af
Joel Hendrix <jhendrix@microsoft.com>
2024-05-01 16:12:47 -0700

    fix tests due to renamed function

--- packages/typespec-rust/test/codegen.test.ts
+++ packages/typespec-rust/test/codegen.test.ts
@@ -4,21 +4,21 @@
 *--------------------------------------------------------------------------------------------*/
 
 import * as rust from '../src/codemodel/index.js';
-import { generateCargoTomlFile } from '../src/codegen/cargotoml.js';
+import { emitCargoToml } from '../src/codegen/cargotoml.js';
 import { strictEqual } from 'assert';
 import { describe, it } from 'vitest';
 
 describe('typespec-rust: codegen', () => {
   describe('generateCargoTomlFile', () => {
     it('default Cargo.toml file', async () => {
-      const cargoToml = generateCargoTomlFile(new rust.Crate('test_crate', '1.2.3'));
+      const cargoToml = emitCargoToml(new rust.Crate('test_crate', '1.2.3', 'azure-arm'));
       strictEqual(cargoToml, '[package]\nname = "test_crate"\nversion = "1.2.3"\nedition.workspace = true\n');
     });
 
     it('default Cargo.toml file with dependencies', async () => {
-      const crate = new rust.Crate('test_crate', '1.2.3');
+      const crate = new rust.Crate('test_crate', '1.2.3', 'data-plane');
       crate.dependencies.push(new rust.CrateDependency('azure_core'));
-      const cargoToml = generateCargoTomlFile(crate);
+      const cargoToml = emitCargoToml(crate);
       strictEqual(cargoToml, '[package]\nname = "test_crate"\nversion = "1.2.3"\nedition.workspace = true\n\n[dependencies]\nazure_core = { workspace = true }\n');
     });
   });
