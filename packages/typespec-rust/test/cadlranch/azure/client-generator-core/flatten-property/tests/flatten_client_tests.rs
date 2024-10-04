87d42a8c0f58398f8e01f257630321ed6ad31326
Joel Hendrix <jhendrix@microsoft.com>
2024-10-04 09:19:58 -0700

    cargo fmt

--- packages/typespec-rust/test/cadlranch/azure/client-generator-core/flatten-property/tests/flatten_client_tests.rs
+++ packages/typespec-rust/test/cadlranch/azure/client-generator-core/flatten-property/tests/flatten_client_tests.rs
@@ -2,7 +2,9 @@
 //
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-use cadl_flattenproperty::models::{ChildFlattenModel, ChildModel, FlattenModel, NestedFlattenModel};
+use cadl_flattenproperty::models::{
+    ChildFlattenModel, ChildModel, FlattenModel, NestedFlattenModel,
+};
 use cadl_flattenproperty::FlattenPropertyClient;
 
 #[async_std::test]
