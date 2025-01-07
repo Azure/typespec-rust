ed0deb63705af050730718570f22b9854b382680
Joel Hendrix <jhendrix@microsoft.com>
2025-01-07 06:20:32 -0800

    Add support for tsp duration types (#208)
    Numeric durations are emitted as their respective numeric types. For
    ISO8601 durations we emit them as String types.

---
+++ packages/typespec-rust/test/cadlranch/encode/duration/tests/duration_header_client_test.rs
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+use cadl_duration::DurationClient;
+
+#[async_std::test]
+async fn default() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .default("P40D".to_string(), None)
+        .await
+        .unwrap();
+}
+
+#[async_std::test]
+async fn float64_seconds() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .float64_seconds(35.625, None)
+        .await
+        .unwrap();
+}
+
+#[async_std::test]
+async fn float_seconds() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .float_seconds(35.625, None)
+        .await
+        .unwrap();
+}
+
+#[async_std::test]
+async fn int32_seconds() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .int32_seconds(36, None)
+        .await
+        .unwrap();
+}
+
+#[async_std::test]
+async fn iso8601() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .iso8601("P40D".to_string(), None)
+        .await
+        .unwrap();
+}
+
+#[async_std::test]
+async fn iso8601_array() {
+    let client = DurationClient::with_no_credential("http://localhost:3000", None).unwrap();
+    client
+        .get_duration_header_client()
+        .iso8601_array(vec!["P40D".to_string(), "P50D".to_string()], None)
+        .await
+        .unwrap();
+}
