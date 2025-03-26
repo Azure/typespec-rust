486704887f6a05bbdafe7e97253d1dd8ffb92423
Joel Hendrix <jhendrix@microsoft.com>
2025-03-26 07:37:33 -0700

    Update storage with latest tsp

--- packages/typespec-rust/test/sdk/blob_storage/src/generated/clients/block_blob_client.rs
+++ packages/typespec-rust/test/sdk/blob_storage/src/generated/clients/block_blob_client.rs
@@ -502,7 +502,7 @@ impl BlockBlobClient {
         url = url.join(&path)?;
         url.query_pairs_mut().append_pair("comp", "block");
         url.query_pairs_mut()
-            .append_pair("blockid", &base64::encode_url_safe(block_id));
+            .append_pair("blockid", &base64::encode(block_id));
         if let Some(timeout) = options.timeout {
             url.query_pairs_mut()
                 .append_pair("timeout", &timeout.to_string());
@@ -581,7 +581,7 @@ impl BlockBlobClient {
             .append_pair("comp", "block")
             .append_key_only("fromURL");
         url.query_pairs_mut()
-            .append_pair("blockid", &base64::encode_url_safe(block_id));
+            .append_pair("blockid", &base64::encode(block_id));
         if let Some(timeout) = options.timeout {
             url.query_pairs_mut()
                 .append_pair("timeout", &timeout.to_string());
