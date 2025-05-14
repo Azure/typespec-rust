7524eedcbc5163c5872e8e407ed1cd1a77716d33
Joel Hendrix <jhendrix@microsoft.com>
2025-05-14 15:05:45 -0700

    Add Spector coverage for client initialization (#411)
    Fixed improper handling of aliased client parameters.
    Replaced OperationParamType with the tcgc equivalent.

---
+++ packages/typespec-rust/test/spector/azure/client-generator-core/client-initialization/tests/parent_child_client.rs
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+use spector_clientinit::{
+    models::{BlobProperties, ParentChildClientWithQueryOptions},
+    ParentChildClient,
+};
+use time::{Date, Month, OffsetDateTime, Time};
+
+#[tokio::test]
+async fn delete_standalone() {
+    let client = ParentChildClient::with_no_credential(
+        "http://localhost:3000",
+        "sample-blob".to_string(),
+        None,
+    )
+    .unwrap();
+    client.delete_standalone(None).await.unwrap();
+}
+
+#[tokio::test]
+async fn get_standalone() {
+    let client = ParentChildClient::with_no_credential(
+        "http://localhost:3000",
+        "sample-blob".to_string(),
+        None,
+    )
+    .unwrap();
+    let resp = client.get_standalone(None).await.unwrap();
+    let blob_properties: BlobProperties = resp.into_body().await.unwrap();
+    assert_eq!(blob_properties.content_type, Some("text/plain".to_string()));
+    assert_eq!(
+        blob_properties.created_on,
+        Some(OffsetDateTime::new_utc(
+            Date::from_calendar_date(2025, Month::April, 1).unwrap(),
+            Time::from_hms(12, 0, 0).unwrap(),
+        ))
+    );
+    assert_eq!(blob_properties.name, Some("sample-blob".to_string()));
+    assert_eq!(blob_properties.size, Some(42));
+}
+
+#[tokio::test]
+async fn with_query() {
+    let client = ParentChildClient::with_no_credential(
+        "http://localhost:3000",
+        "sample-blob".to_string(),
+        None,
+    )
+    .unwrap();
+    client
+        .with_query(Some(ParentChildClientWithQueryOptions {
+            format: Some("text".to_string()),
+            ..Default::default()
+        }))
+        .await
+        .unwrap();
+}
