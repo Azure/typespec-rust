ed9f079e190e2445763cc7e86fdacd6251d1dad3
Joel Hendrix <jhendrix@microsoft.com>
2025-05-28 06:23:36 -0700

    Fix handling of required API version param (#428)
    Added more pending Spector version tests (commented out).

---
+++ packages/typespec-rust/test/spector/versioning/madeOptional/tests/made_optional_client.rs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+use spector_madeoptional::{models::TestModel, MadeOptionalClient};
+
+#[tokio::test]
+async fn test() {
+    let client =
+        MadeOptionalClient::with_no_credential("http://localhost:3000", "v2", None).unwrap();
+    let body = TestModel {
+        prop: Some("foo".to_string()),
+        ..Default::default()
+    };
+    let resp = client
+        .test(body.try_into().unwrap(), None)
+        .await
+        .unwrap()
+        .into_body()
+        .await
+        .unwrap();
+    assert_eq!(resp.prop, Some("foo".to_string()));
+}
