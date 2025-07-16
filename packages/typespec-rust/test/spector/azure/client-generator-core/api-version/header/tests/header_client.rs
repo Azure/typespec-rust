7b168395890d733f6d0aa5cdf169323461c532b3
Charles Lowell <10964656+chlowell@users.noreply.github.com>
2025-07-16 07:44:06 -0700

    unrelated clippy error

--- packages/typespec-rust/test/spector/azure/client-generator-core/api-version/header/tests/header_client.rs
+++ packages/typespec-rust/test/spector/azure/client-generator-core/api-version/header/tests/header_client.rs
@@ -9,7 +9,7 @@ async fn header_api_version() {
     let client = HeaderClient::with_no_credential("http://localhost:3000", None).unwrap();
     let res = match client.header_api_version(None).await {
         Ok(response) => response,
-        Err(e) => panic!("Request failed: {}", e),
+        Err(e) => panic!("Request failed: {e}"),
     };
     assert_eq!(res.status(), 200);
 }
