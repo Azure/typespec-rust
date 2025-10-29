0bf15235448415d7e55d795e09c05ca5a3dfeed8
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2025-10-29 13:03:45 -0700

    Add export test

--- packages/typespec-rust/test/spector/azure/core/lro/standard/tests/standard_client_test.rs
+++ packages/typespec-rust/test/spector/azure/core/lro/standard/tests/standard_client_test.rs
@@ -8,7 +8,10 @@ use azure_core::time::Duration;
 use futures::StreamExt;
 
 use spector_lrostd::{
-    models::{StandardClientCreateOrReplaceOptions, StandardClientDeleteOptions, User},
+    models::{
+        StandardClientCreateOrReplaceOptions, StandardClientDeleteOptions,
+        StandardClientExportOptions, User,
+    },
     StandardClient,
 };
 
@@ -92,6 +95,55 @@ async fn delete() {
             3 => {
                 assert_eq!(status, StatusCode::Ok);
                 assert_eq!(result.status(), PollerStatus::Succeeded);
+            }
+            _ => {
+                panic!("unexpected poll count");
+            }
+        }
+    }
+}
+
+#[tokio::test]
+async fn export() {
+    let client = StandardClient::with_no_credential("http://localhost:3000", None).unwrap();
+    let mut poller = client
+        .export(
+            "madge",
+            "json",
+            Some(StandardClientExportOptions {
+                poller_options: PollerOptions {
+                    frequency: Some(Duration::seconds(1)),
+                },
+                ..Default::default()
+            }),
+        )
+        .unwrap();
+
+    let mut poll_count = 0;
+    while let Some(result) = poller.next().await {
+        poll_count += 1;
+        let result = result.unwrap();
+        let status = result.status();
+        let result = result.into_body().unwrap();
+        match poll_count {
+            1 => {
+                assert_eq!(status, StatusCode::Accepted);
+                assert_eq!(result.status(), PollerStatus::InProgress);
+            }
+            2 => {
+                assert_eq!(status, StatusCode::Ok);
+                assert_eq!(result.status(), PollerStatus::InProgress);
+            }
+            3 => {
+                assert_eq!(status, StatusCode::Ok);
+                assert_eq!(result.status(), PollerStatus::Succeeded);
+                match result.result {
+                    Some(user) => {
+                        assert_eq!(user.name, Some("madge".to_string()));
+                        assert_eq!(user.resource_uri, Some("/users/madge".to_string()));
+                    }
+                    None => panic!("expected result"),
+                }
             }
             _ => {
                 panic!("unexpected poll count");
