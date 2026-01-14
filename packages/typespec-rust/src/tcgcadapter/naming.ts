7e7e1438dabac2b76b05a43447cf7d19ccc8a5d8
Joel Hendrix <jhendrix@microsoft.com>
2026-01-13 17:13:57 -0800

    wip

--- packages/typespec-rust/src/tcgcadapter/naming.ts
+++ packages/typespec-rust/src/tcgcadapter/naming.ts
@@ -5,6 +5,7 @@
 
 import * as codegen from '@azure-tools/codegen';
 import * as tcgc from '@azure-tools/typespec-client-generator-core';
+import * as tsp from '@typespec/compiler';
 
 /**
  * if name is a reserved word, append the suffix and return the result, else return name.
@@ -75,4 +76,93 @@ export function fixUpEnumValueNameWorker(name: string): string {
   }
 
   return fixedName;
+}
+
+/**
+ * caches Rust names created from tcgc artifacts.
+ * any names that result in duplicates will have
+ * a COLLIDES_* prefix.
+ */
+export class NameCache<T extends tcgc.SdkType> {
+  private readonly program: tsp.Program;
+  private readonly nameToSdkEntries;
+  private readonly sdkTypeToName;
+  private readonly groupAndCount;
+  private groupCounter: number;
+
+  constructor(program: tsp.Program) {
+    this.program = program;
+    this.nameToSdkEntries = new Map<string, Array<T>>();
+    this.sdkTypeToName = new Map<T, string>();
+    this.groupAndCount = new Map<string, {group: number, count: number}>();
+    this.groupCounter = 1;
+  }
+
+  /**
+   * adds a mapping between a Rust name and the instance of a tcgc SDK type.
+   * 
+   * @param name 
+   * @param sdkType 
+   */
+  add(name: string, sdkType: T): void {
+    let existingMapping = this.nameToSdkEntries.get(name);
+    if (!existingMapping) {
+      existingMapping = new Array<T>();
+      this.nameToSdkEntries.set(name, existingMapping);
+    } else {
+      // have collisions, add tracking group and count info as needed
+      if (!this.groupAndCount.has(name)) {
+        this.groupAndCount.set(name, {group: this.groupCounter, count: 0});
+        ++this.groupCounter;
+      }
+    }
+    existingMapping.push(sdkType);
+
+    if (this.sdkTypeToName.has(sdkType)) {
+      throw new Error('todo dupe');
+    }
+    this.sdkTypeToName.set(sdkType, name);
+  }
+
+  /**
+   * gets the Rust name for the provided SDK type instance.
+   * when name maps to more than one instance of the associated
+   * SDK type, the COLLIDES_* prefix is included in the name.
+   * 
+   * @param sdkType 
+   * @returns 
+   */
+  get(sdkType: T): string {
+    const name = this.sdkTypeToName.get(sdkType);
+    if (!name) {
+      throw new Error('todo name');
+    }
+
+    const entriesForName = this.nameToSdkEntries.get(name);
+    if (!entriesForName) {
+      throw new Error('todo entries');
+    }
+
+    if (entriesForName.length === 1) {
+      // no collisions
+      this.sdkTypeToName.delete(sdkType);
+      this.nameToSdkEntries.delete(name);
+      return name;
+    }
+
+    const groupCountInfo = this.groupAndCount.get(name);
+    if (!groupCountInfo) {
+      throw new Error('todo info');
+    }
+
+    this.program.reportDiagnostic({
+      code: 'NameCollision',
+      severity: 'warning',
+      message: `duplicate symbol name ${name}`,
+      target: sdkType.__raw?.node ?? tsp.NoTarget,
+    });
+
+    ++groupCountInfo.count;
+    return `COLLIDES_GRP${groupCountInfo.group}_ID${groupCountInfo.count}_${name}`;
+  }
 }
