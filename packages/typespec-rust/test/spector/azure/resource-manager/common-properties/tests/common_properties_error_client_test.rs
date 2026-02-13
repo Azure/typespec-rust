f0ccaf9a3137b4f43a93dd5cbd3f690d477a7204
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2026-02-13 01:47:21 -0800

    Verify error messages in the test

--- packages/typespec-rust/test/spector/azure/resource-manager/common-properties/tests/common_properties_error_client_test.rs
+++ packages/typespec-rust/test/spector/azure/resource-manager/common-properties/tests/common_properties_error_client_test.rs
@@ -5,14 +5,28 @@
 mod common;
 
 use azure_core::http::StatusCode;
+use azure_core::Error;
 use spector_armcommon::models::{ConfidentialResource, ConfidentialResourceProperties};
+
+#[tokio::test]
+async fn get_for_predefined_error() {
+    let client = common::create_client();
+    let rsp = client
+        .get_common_properties_error_client()
+        .get_for_predefined_error("test-rg", "resource", None)
+        .await;
+
+    let err = rsp.unwrap_err();
+    assert_eq!(err.http_status(), Some(StatusCode::NotFound));
+    assert_eq!(err.to_string(), "The Resource 'Azure.ResourceManager.CommonProperties/confidentialResources/confidential' under resource group 'test-rg' was not found.");
+}
 
 #[tokio::test]
 async fn create_for_user_defined_error() {
     let resource = ConfidentialResource {
         location: Some("eastus".to_string()),
         properties: Some(ConfidentialResourceProperties {
-            username: Some("testuser".to_string()),
+            username: Some("00".to_string()),
             ..Default::default()
         }),
         ..Default::default()
@@ -24,16 +38,7 @@ async fn create_for_user_defined_error() {
         .create_for_user_defined_error("test-rg", "resource", resource.try_into().unwrap(), None)
         .await;
 
-    assert_eq!(rsp.unwrap_err().http_status(), Some(StatusCode::BadRequest));
-}
-
-#[tokio::test]
-async fn get_for_predefined_error() {
-    let client = common::create_client();
-    let rsp = client
-        .get_common_properties_error_client()
-        .get_for_predefined_error("test-rg", "resource", None)
-        .await;
-
-    assert_eq!(rsp.unwrap_err().http_status(), Some(StatusCode::NotFound));
+    let err = rsp.unwrap_err();
+    assert_eq!(err.http_status(), Some(StatusCode::BadRequest));
+    assert_eq!(err.to_string(), "Username should not contain only numbers.");
 }
