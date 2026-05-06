Azure/typespec-rust

commit 7237cce40ba629dc17b43480f7df017b94330887

Use the parameter's access when adapting the options field (#942)
It's possible for optional parameters to be pub(crate), so use its
authoring instead of using the method's visibility.
Add missing changelog entry for cb3e1ce and update version.

packages/typespec-rust/src/codegen/clients.ts
@@ -449,7 +449,7 @@ function getMethodOptions(module: rust.ModuleContainer): helpers.Module | undefi
         if (fieldDocs.length > 0) {
           block += `${indent.get()}${fieldDocs}`;
         }
-        block += `${indent.get()}${helpers.emitVisibility(method.visibility)}${field.name}: ${helpers.getTypeDeclaration(field.type)},\n`;
+        block += `${indent.get()}${helpers.emitVisibility(field.visibility)}${field.name}: ${helpers.getTypeDeclaration(field.type)},\n`;
         if (i + 1 < optionsStruct.fields.length) {
           block += '\n';
         }

packages/typespec-rust/test/sdk/blob_storage/src/generated/models/method_options.rs
@@ -164,7 +164,7 @@ pub struct AppendBlobClientAppendBlockOptions<'a> {
     pub method_options: ClientMethodOptions<'a>,
 
     /// Required if the request body is a structured message. Specifies the message schema version and properties.
-    pub structured_body_type: Option<String>,
+    pub(crate) structured_body_type: Option<String>,
 
     /// Required if the request body is a structured message. Specifies the length of the blob/file content inside the message
     /// body. Will always be smaller than Content-Length.
@@ -548,7 +548,7 @@ pub struct BlobClientDownloadInternalOptions<'a> {
 
     /// Specifies the response content should be returned as a structured message and specifies the message schema version and
     /// properties.
-    pub structured_body_type: Option<String>,
+    pub(crate) structured_body_type: Option<String>,
 
     /// The timeout parameter is expressed in seconds. For more information, see [Setting Timeouts for Blob Service Operations.](https://docs.microsoft.com/en-us/rest/api/storageservices/fileservices/setting-timeouts-for-blob-service-operations)
     pub timeout: Option<i32>,
@@ -1509,7 +1509,7 @@ pub struct BlockBlobClientStageBlockOptions<'a> {
     pub method_options: ClientMethodOptions<'a>,
 
     /// Required if the request body is a structured message. Specifies the message schema version and properties.
-    pub structured_body_type: Option<String>,
+    pub(crate) structured_body_type: Option<String>,
 
     /// Required if the request body is a structured message. Specifies the length of the blob/file content inside the message
     /// body. Will always be smaller than Content-Length.
@@ -1727,7 +1727,7 @@ pub struct BlockBlobClientUploadInternalOptions<'a> {
     pub method_options: ClientMethodOptions<'a>,
 
     /// Required if the request body is a structured message. Specifies the message schema version and properties.
-    pub structured_body_type: Option<String>,
+    pub(crate) structured_body_type: Option<String>,
 
     /// Required if the request body is a structured message. Specifies the length of the blob/file content inside the message
     /// body. Will always be smaller than Content-Length.
@@ -2152,7 +2152,7 @@ pub struct PageBlobClientUploadPagesOptions<'a> {
     pub method_options: ClientMethodOptions<'a>,
 
     /// Required if the request body is a structured message. Specifies the message schema version and properties.
-    pub structured_body_type: Option<String>,
+    pub(crate) structured_body_type: Option<String>,
 
     /// Required if the request body is a structured message. Specifies the length of the blob/file content inside the message
     /// body. Will always be smaller than Content-Length.

packages/typespec-rust/src/tcgcadapter/adapter.ts
@@ -2024,7 +2024,7 @@ export class Adapter {
           fieldType = this.getOptionType(adaptedParam.type);
         }
 
-        const optionsField = new rust.StructField(adaptedParam.name, pub, fieldType);
+        const optionsField = new rust.StructField(adaptedParam.name, adaptAccessFlags(methodParam.access), fieldType);
         optionsField.docs = adaptedParam.docs;
         rustMethod.options.type.type.fields.push(optionsField);
       }

commit f1fb3b2909d85bd1c72ec086a986423e5e046daa

Fixes for internal and XML helpers
When emitting access specifiers for fields, if the containing type is
less visible than the field, use the containing type's visibility. This
fixes a regression introduced in 7237cce.
When creating the module name for XML serde helpers, use the model name
instead of the XML tag name as the model name is guaranteed to be
unique. This prevents using the incorrect helper for types when they are
discrete but use the same XML tag name.

packages/typespec-rust/src/codegen/clients.ts
@@ -451,7 +451,12 @@ function getMethodOptions(module: rust.ModuleContainer): helpers.Module | undefi
         if (fieldDocs.length > 0) {
           block += `${indent.get()}${fieldDocs}`;
         }
-        block += `${indent.get()}${helpers.emitVisibility(field.visibility)}${field.name}: ${helpers.getTypeDeclaration(field.type)},\n`;
+        let visibilityToEmit = field.visibility;
+        // if the method has less visibility that the field, prefer that
+        if (method.visibility === 'pubCrate') {
+          visibilityToEmit = method.visibility;
+        }
+        block += `${indent.get()}${helpers.emitVisibility(visibilityToEmit)}${field.name}: ${helpers.getTypeDeclaration(field.type)},\n`;
         if (i + 1 < optionsStruct.fields.length) {
           block += '\n';
         }

packages/typespec-rust/src/codegen/models.ts
@@ -434,6 +434,7 @@ function getXMLListWrapper(field: rust.ModelField): XMLListWrapper {
   // scalar types use the TypeSpec defined names. so, we need
   // to translate from Rust scalar types back to TypeSpec.
   let unwrappedFieldTypeName: string;
+  let underlyingModelName: string | undefined;
   const wrappedType = helpers.unwrapType(field.type);
   switch (wrappedType.kind) {
     case 'String':
@@ -442,6 +443,10 @@ function getXMLListWrapper(field: rust.ModelField): XMLListWrapper {
     case 'model':
       if (wrappedType.xmlName) {
         unwrappedFieldTypeName = wrappedType.xmlName;
+        // it's possible for multiple models to use the same XML tag over
+        // the wire. so instead of using the tag name for the helper name, we
+        // use the model name which is guaranteed to be unique.
+        underlyingModelName = wrappedType.name;
       } else {
         unwrappedFieldTypeName = wrappedType.name;
       }
@@ -487,9 +492,14 @@ function getXMLListWrapper(field: rust.ModelField): XMLListWrapper {
       unwrappedFieldTypeName = helpers.getTypeDeclaration(wrappedType);
   }
 
+  let helperNameSuffx = unwrappedFieldTypeName;
+  if (underlyingModelName) {
+    helperNameSuffx = underlyingModelName;
+  }
+
   // the wrapper type name is a combination of the field name and the
   // unwrapped type name of T. this is to ensure unique type names
-  const wrapperTypeName = `${helpers.capitalize(field.name)}${helpers.capitalize(unwrappedFieldTypeName)}`;
+  const wrapperTypeName = `${helpers.capitalize(field.name)}${helpers.capitalize(helperNameSuffx)}`;
   let xmlListWrapper = xmlListWrappers.get(wrapperTypeName);
   if (!xmlListWrapper) {
     xmlListWrapper = new XMLListWrapper(wrapperTypeName, unwrappedFieldTypeName, field.type);

packages/typespec-rust/test/other/misc_tests/src/generated/clients/misc_tests_client.rs
@@ -4,16 +4,18 @@
 // Code generated by Microsoft (R) Rust Code Generator. DO NOT EDIT.
 
 use crate::generated::models::{
-    ChatRequestMessage, Colors, DiscriminatedBaseNoSubTypes, FoundWidgets, GroupedParams, Indices,
-    LiteralWithInvalidChar, MiscTestsClientAvoidDupeHeadersOneOptions,
-    MiscTestsClientAvoidDupeHeadersOneResult, MiscTestsClientAvoidDupeHeadersTwoOptions,
-    MiscTestsClientAvoidDupeHeadersTwoResult, MiscTestsClientCollidingOptionsParamOptions,
-    MiscTestsClientEtagHeaderParameterOptions, MiscTestsClientFindTheWidgetsOptions,
-    MiscTestsClientGetDiscriminatedNoSubTypesOptions, MiscTestsClientGetUnionsWithCyclesOptions,
-    MiscTestsClientLiteralWithInvalidCharOptions, MiscTestsClientParamGroupOptions,
-    MiscTestsClientRequiredPathParamNoEmptyCheckOptions, MiscTestsClientSpreadParamWithEnumOptions,
-    MiscTestsClientVariousExplodedQueryParamsOptions, MiscTestsClientWithClientParamsOptions,
-    MiscTestsClientWithOptionalClientQueryParamOptions, SpreadWithEnum,
+    ArrayOfTypeOne, ArrayOfTypeTwo, ChatRequestMessage, Colors, DiscriminatedBaseNoSubTypes,
+    FoundWidgets, GroupedParams, Indices, LiteralWithInvalidChar,
+    MiscTestsClientAvoidDupeHeadersOneOptions, MiscTestsClientAvoidDupeHeadersOneResult,
+    MiscTestsClientAvoidDupeHeadersTwoOptions, MiscTestsClientAvoidDupeHeadersTwoResult,
+    MiscTestsClientCollidingOptionsParamOptions, MiscTestsClientEtagHeaderParameterOptions,
+    MiscTestsClientFindTheWidgetsOptions, MiscTestsClientGetArrayOfTypeOneOptions,
+    MiscTestsClientGetArrayOfTypeTwoOptions, MiscTestsClientGetDiscriminatedNoSubTypesOptions,
+    MiscTestsClientGetUnionsWithCyclesOptions, MiscTestsClientLiteralWithInvalidCharOptions,
+    MiscTestsClientParamGroupOptions, MiscTestsClientRequiredPathParamNoEmptyCheckOptions,
+    MiscTestsClientSpreadParamWithEnumOptions, MiscTestsClientVariousExplodedQueryParamsOptions,
+    MiscTestsClientWithClientParamsOptions, MiscTestsClientWithOptionalClientQueryParamOptions,
+    SpreadWithEnum,
 };
 use azure_core::{
     error::CheckSuccessOptions,
@@ -21,7 +23,7 @@ use azure_core::{
     http::{
         pager::{PagerContinuation, PagerResult, PagerState},
         ClientOptions, Etag, Method, NoFormat, Pager, Pipeline, PipelineSendOptions, RawResponse,
-        Request, RequestContent, Response, Url, UrlExt,
+        Request, RequestContent, Response, Url, UrlExt, XmlFormat,
     },
     json, tracing, Result,
 };
@@ -315,6 +317,68 @@ impl MiscTestsClient {
             },
             Some(options.method_options),
         ))
+    }
+
+    ///
+    /// # Arguments
+    ///
+    /// * `options` - Optional parameters for the request.
+    #[tracing::function("MiscTests.getArrayOfTypeOne")]
+    pub async fn get_array_of_type_one(
+        &self,
+        options: Option<MiscTestsClientGetArrayOfTypeOneOptions<'_>>,
+    ) -> Result<Response<ArrayOfTypeOne, XmlFormat>> {
+        let options = options.unwrap_or_default();
+        let ctx = options.method_options.context.to_borrowed();
+        let mut url = self.endpoint.clone();
+        url.append_path("/get-array-of-type-one");
+        let mut request = Request::new(url, Method::Get);
+        request.insert_header("accept", "application/xml");
+        let rsp = self
+            .pipeline
+            .send(
+                &ctx,
+                &mut request,
+                Some(PipelineSendOptions {
+                    check_success: CheckSuccessOptions {
+                        success_codes: &[200],
+                    },
+                    ..Default::default()
+                }),
+            )
+            .await?;
+        Ok(rsp.into())
+    }
+
+    ///
+    /// # Arguments
+    ///
+    /// * `options` - Optional parameters for the request.
+    #[tracing::function("MiscTests.getArrayOfTypeTwo")]
+    pub async fn get_array_of_type_two(
+        &self,
+        options: Option<MiscTestsClientGetArrayOfTypeTwoOptions<'_>>,
+    ) -> Result<Response<ArrayOfTypeTwo, XmlFormat>> {
+        let options = options.unwrap_or_default();
+        let ctx = options.method_options.context.to_borrowed();
+        let mut url = self.endpoint.clone();
+        url.append_path("/get-array-of-type-two");
+        let mut request = Request::new(url, Method::Get);
+        request.insert_header("accept", "application/xml");
+        let rsp = self
+            .pipeline
+            .send(
+                &ctx,
+                &mut request,
+                Some(PipelineSendOptions {
+                    check_success: CheckSuccessOptions {
+                        success_codes: &[200],
+                    },
+                    ..Default::default()
+                }),
+            )
+            .await?;
+        Ok(rsp.into())
     }
 
     ///

packages/typespec-rust/test/other/misc_tests/src/generated/models/method_options.rs
@@ -69,6 +69,20 @@ impl MiscTestsClientFindTheWidgetsOptions<'_> {
     }
 }
 
+/// Options to be passed to [`MiscTestsClient::get_array_of_type_one()`](crate::generated::clients::MiscTestsClient::get_array_of_type_one())
+#[derive(Clone, Default, SafeDebug)]
+pub struct MiscTestsClientGetArrayOfTypeOneOptions<'a> {
+    /// Allows customization of the method call.
+    pub method_options: ClientMethodOptions<'a>,
+}
+
+/// Options to be passed to [`MiscTestsClient::get_array_of_type_two()`](crate::generated::clients::MiscTestsClient::get_array_of_type_two())
+#[derive(Clone, Default, SafeDebug)]
+pub struct MiscTestsClientGetArrayOfTypeTwoOptions<'a> {
+    /// Allows customization of the method call.
+    pub method_options: ClientMethodOptions<'a>,
+}
+
 /// Options to be passed to [`MiscTestsClient::get_discriminated_no_sub_types()`](crate::generated::clients::MiscTestsClient::get_discriminated_no_sub_types())
 #[derive(Clone, Default, SafeDebug)]
 pub struct MiscTestsClientGetDiscriminatedNoSubTypesOptions<'a> {

packages/typespec-rust/test/other/misc_tests/src/generated/models/mod.rs
@@ -14,6 +14,7 @@ mod models_impl;
 mod models_serde;
 mod unions;
 mod unions_serde;
+mod xml_helpers;
 pub use enums::*;
 pub use header_traits::*;
 pub use method_options::*;

packages/typespec-rust/test/other/misc_tests/src/generated/models/models.rs
@@ -3,9 +3,39 @@
 //
 // Code generated by Microsoft (R) Rust Code Generator. DO NOT EDIT.
 
-use super::{models_serde, ChatRequestUserMessageContent, Colors, Indices};
+use super::{
+    models_serde,
+    xml_helpers::{ItemsTypeOne, ItemsTypeTwo},
+    ChatRequestUserMessageContent, Colors, Indices,
+};
 use azure_core::fmt::SafeDebug;
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Default, Deserialize, SafeDebug, Serialize)]
+#[non_exhaustive]
+pub struct ArrayOfTypeOne {
+    #[serde(
+        default,
+        deserialize_with = "ItemsTypeOne::unwrap",
+        rename = "Items",
+        serialize_with = "ItemsTypeOne::wrap",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub items: Option<Vec<TypeOne>>,
+}
+
+#[derive(Clone, Default, Deserialize, SafeDebug, Serialize)]
+#[non_exhaustive]
+pub struct ArrayOfTypeTwo {
+    #[serde(
+        default,
+        deserialize_with = "ItemsTypeTwo::unwrap",
+        rename = "Items",
+        serialize_with = "ItemsTypeTwo::wrap",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub items: Option<Vec<TypeTwo>>,
+}
 
 #[derive(Clone, Default, Deserialize, SafeDebug, Serialize)]
 #[non_exhaustive]
@@ -80,6 +110,22 @@ pub(crate) struct SpreadWithEnum {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) sides: Option<i32>,
+}
+
+#[derive(Clone, Default, Deserialize, SafeDebug, Serialize)]
+#[non_exhaustive]
+#[serde(rename = "Widget")]
+pub struct TypeOne {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+}
+
+#[derive(Clone, Default, Deserialize, SafeDebug, Serialize)]
+#[non_exhaustive]
+#[serde(rename = "Widget")]
+pub struct TypeTwo {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub size: Option<i32>,
 }
 
 #[derive(Serialize)]

packages/typespec-rust/test/other/misc_tests/src/generated/models/xml_helpers.rs
@@ -1,0 +1,62 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Code generated by Microsoft (R) Rust Code Generator. DO NOT EDIT.
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use super::{TypeOne, TypeTwo};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename = "Items")]
+pub(crate) struct ItemsTypeOne {
+    #[serde(default)]
+    Widget: Option<Vec<TypeOne>>,
+}
+
+impl ItemsTypeOne {
+    pub fn unwrap<'de, D>(deserializer: D) -> Result<Option<Vec<TypeOne>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(ItemsTypeOne::deserialize(deserializer)?.Widget)
+    }
+
+    pub fn wrap<S>(to_serialize: &Option<Vec<TypeOne>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        ItemsTypeOne {
+            Widget: to_serialize.to_owned(),
+        }
+        .serialize(serializer)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename = "Items")]
+pub(crate) struct ItemsTypeTwo {
+    #[serde(default)]
+    Widget: Option<Vec<TypeTwo>>,
+}
+
+impl ItemsTypeTwo {
+    pub fn unwrap<'de, D>(deserializer: D) -> Result<Option<Vec<TypeTwo>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(ItemsTypeTwo::deserialize(deserializer)?.Widget)
+    }
+
+    pub fn wrap<S>(to_serialize: &Option<Vec<TypeTwo>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        ItemsTypeTwo {
+            Widget: to_serialize.to_owned(),
+        }
+        .serialize(serializer)
+    }
+}

packages/typespec-rust/test/other/pub_crate/src/generated/clients/misc_tests_client.rs
@@ -10,6 +10,7 @@ use azure_core::{
     http::{
         pager::{PagerContinuation, PagerResult, PagerState},
         ClientOptions, Method, Pager, Pipeline, PipelineSendOptions, RawResponse, Request, Url,
+        UrlExt,
     },
     json, tracing, Result,
 };
@@ -76,7 +77,12 @@ impl MiscTestsClient {
     ) -> Result<Pager<PagedWidget>> {
         let options = options.unwrap_or_default().into_owned();
         let pipeline = self.pipeline.clone();
-        let first_url = self.endpoint.clone();
+        let mut first_url = self.endpoint.clone();
+        let mut query_builder = first_url.query_builder();
+        if let Some(count) = options.count {
+            query_builder.set_pair("count", count.to_string());
+        }
+        query_builder.build();
         Ok(Pager::new(
             move |next_link: PagerState, pager_options| {
                 let url = match next_link {

packages/typespec-rust/test/other/pub_crate/src/generated/models/method_options.rs
@@ -8,6 +8,8 @@ use azure_core::{fmt::SafeDebug, http::pager::PagerOptions};
 /// Options to be passed to [`MiscTestsClient::list_widgets()`](crate::generated::clients::MiscTestsClient::list_widgets())
 #[derive(Clone, Default, SafeDebug)]
 pub(crate) struct MiscTestsClientListWidgetsOptions<'a> {
+    pub(crate) count: Option<i32>,
+
     /// Allows customization of the method call.
     pub(crate) method_options: PagerOptions<'a>,
 }
@@ -16,6 +18,7 @@ impl MiscTestsClientListWidgetsOptions<'_> {
     /// Transforms this [`MiscTestsClientListWidgetsOptions`] into a new `MiscTestsClientListWidgetsOptions` that owns the underlying data, cloning it if necessary.
     pub fn into_owned(self) -> MiscTestsClientListWidgetsOptions<'static> {
         MiscTestsClientListWidgetsOptions {
+            count: self.count,
             method_options: PagerOptions {
                 context: self.method_options.context.into_owned(),
                 ..self.method_options

packages/typespec-rust/test/sdk/blob_storage/src/generated/models/models.rs
@@ -6,8 +6,8 @@
 use super::{
     models_serde,
     xml_helpers::{
-        Blob_tag_setTag, BlobsBlob, Committed_blocksBlock, Container_itemsContainer, CorsCorsRule,
-        Uncommitted_blocksBlock,
+        Blob_tag_setBlobTag, BlobsFilterBlobItem, Committed_blocksBlock,
+        Container_itemsContainerItem, CorsCorsRule, Uncommitted_blocksBlock,
     },
     AccessTier, ArchiveStatus, BlobType, CopyStatus, GeoReplicationStatusType,
     ImmutabilityPolicyMode, LeaseDuration, LeaseState, LeaseStatus, PublicAccessType,
@@ -508,9 +508,9 @@ pub struct BlobTags {
     /// Represents the blob tags.
     #[serde(
         default,
-        deserialize_with = "Blob_tag_setTag::unwrap",
+        deserialize_with = "Blob_tag_setBlobTag::unwrap",
         rename = "TagSet",
-        serialize_with = "Blob_tag_setTag::wrap",
+        serialize_with = "Blob_tag_setBlobTag::wrap",
         skip_serializing_if = "Option::is_none"
     )]
     pub blob_tag_set: Option<Vec<BlobTag>>,
@@ -847,9 +847,9 @@ pub struct FilterBlobSegment {
     /// The blob segment.
     #[serde(
         default,
-        deserialize_with = "BlobsBlob::unwrap",
+        deserialize_with = "BlobsFilterBlobItem::unwrap",
         rename = "Blobs",
-        serialize_with = "BlobsBlob::wrap",
+        serialize_with = "BlobsFilterBlobItem::wrap",
         skip_serializing_if = "Option::is_none"
     )]
     pub blobs: Option<Vec<FilterBlobItem>>,
@@ -928,9 +928,9 @@ pub struct ListContainersSegmentResponse {
     /// The container segment.
     #[serde(
         default,
-        deserialize_with = "Container_itemsContainer::unwrap",
+        deserialize_with = "Container_itemsContainerItem::unwrap",
         rename = "Containers",
-        serialize_with = "Container_itemsContainer::wrap"
+        serialize_with = "Container_itemsContainerItem::wrap"
     )]
     pub container_items: Vec<ContainerItem>,
 

packages/typespec-rust/test/sdk/blob_storage/src/generated/models/xml_helpers.rs
@@ -11,24 +11,24 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename = "TagSet")]
-pub(crate) struct Blob_tag_setTag {
+pub(crate) struct Blob_tag_setBlobTag {
     #[serde(default)]
     Tag: Option<Vec<BlobTag>>,
 }
 
-impl Blob_tag_setTag {
+impl Blob_tag_setBlobTag {
     pub fn unwrap<'de, D>(deserializer: D) -> Result<Option<Vec<BlobTag>>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        Ok(Blob_tag_setTag::deserialize(deserializer)?.Tag)
+        Ok(Blob_tag_setBlobTag::deserialize(deserializer)?.Tag)
     }
 
     pub fn wrap<S>(to_serialize: &Option<Vec<BlobTag>>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        Blob_tag_setTag {
+        Blob_tag_setBlobTag {
             Tag: to_serialize.to_owned(),
         }
         .serialize(serializer)
@@ -37,17 +37,17 @@ impl Blob_tag_setTag {
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename = "Blobs")]
-pub(crate) struct BlobsBlob {
+pub(crate) struct BlobsFilterBlobItem {
     #[serde(default)]
     Blob: Option<Vec<FilterBlobItem>>,
 }
 
-impl BlobsBlob {
+impl BlobsFilterBlobItem {
     pub fn unwrap<'de, D>(deserializer: D) -> Result<Option<Vec<FilterBlobItem>>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        Ok(BlobsBlob::deserialize(deserializer)?.Blob)
+        Ok(BlobsFilterBlobItem::deserialize(deserializer)?.Blob)
     }
 
     pub fn wrap<S>(
@@ -57,7 +57,7 @@ impl BlobsBlob {
     where
         S: Serializer,
     {
-        BlobsBlob {
+        BlobsFilterBlobItem {
             Blob: to_serialize.to_owned(),
         }
         .serialize(serializer)
@@ -92,24 +92,24 @@ impl Committed_blocksBlock {
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename = "Containers")]
-pub(crate) struct Container_itemsContainer {
+pub(crate) struct Container_itemsContainerItem {
     #[serde(default)]
     Container: Vec<ContainerItem>,
 }
 
-impl Container_itemsContainer {
+impl Container_itemsContainerItem {
     pub fn unwrap<'de, D>(deserializer: D) -> Result<Vec<ContainerItem>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        Ok(Container_itemsContainer::deserialize(deserializer)?.Container)
+        Ok(Container_itemsContainerItem::deserialize(deserializer)?.Container)
     }
 
     pub fn wrap<S>(to_serialize: &Vec<ContainerItem>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        Container_itemsContainer {
+        Container_itemsContainerItem {
             Container: to_serialize.to_owned(),
         }
         .serialize(serializer)

packages/typespec-rust/test/spector/payload/pageable/src/generated/models/models.rs
@@ -3,7 +3,7 @@
 //
 // Code generated by Microsoft (R) Rust Code Generator. DO NOT EDIT.
 
-use super::xml_helpers::PetsPet;
+use super::xml_helpers::PetsXmlPet;
 use azure_core::fmt::SafeDebug;
 use serde::{Deserialize, Serialize};
 
@@ -53,9 +53,9 @@ pub struct XmlPetListResult {
 
     #[serde(
         default,
-        deserialize_with = "PetsPet::unwrap",
+        deserialize_with = "PetsXmlPet::unwrap",
         rename = "Pets",
-        serialize_with = "PetsPet::wrap"
+        serialize_with = "PetsXmlPet::wrap"
     )]
     pub pets: Vec<XmlPet>,
 }
@@ -70,9 +70,9 @@ pub struct XmlPetListResultWithNextLink {
 
     #[serde(
         default,
-        deserialize_with = "PetsPet::unwrap",
+        deserialize_with = "PetsXmlPet::unwrap",
         rename = "Pets",
-        serialize_with = "PetsPet::wrap"
+        serialize_with = "PetsXmlPet::wrap"
     )]
     pub pets: Vec<XmlPet>,
 }

packages/typespec-rust/test/spector/payload/pageable/src/generated/models/xml_helpers.rs
@@ -11,24 +11,24 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename = "Pets")]
-pub(crate) struct PetsPet {
+pub(crate) struct PetsXmlPet {
     #[serde(default)]
     Pet: Vec<XmlPet>,
 }
 
-impl PetsPet {
+impl PetsXmlPet {
     pub fn unwrap<'de, D>(deserializer: D) -> Result<Vec<XmlPet>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        Ok(PetsPet::deserialize(deserializer)?.Pet)
+        Ok(PetsXmlPet::deserialize(deserializer)?.Pet)
     }
 
     pub fn wrap<S>(to_serialize: &Vec<XmlPet>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        PetsPet {
+        PetsXmlPet {
             Pet: to_serialize.to_owned(),
         }
         .serialize(serializer)

packages/typespec-rust/test/spector/payload/xml/src/generated/models/models.rs
@@ -5,7 +5,7 @@
 
 use super::{
     models_serde,
-    xml_helpers::{BooksXmlBook, ColorsString, CountsInt32, ItemsSimpleModel, TagsString},
+    xml_helpers::{BooksBook, ColorsString, CountsInt32, ItemsSimpleModel, TagsString},
     Status,
 };
 use azure_core::{fmt::SafeDebug, time::OffsetDateTime};
@@ -225,9 +225,9 @@ pub struct ModelWithRenamedUnwrappedModelArray {
 pub struct ModelWithRenamedWrappedAndItemModelArray {
     #[serde(
         default,
-        deserialize_with = "BooksXmlBook::unwrap",
+        deserialize_with = "BooksBook::unwrap",
         rename = "AllBooks",
-        serialize_with = "BooksXmlBook::wrap",
+        serialize_with = "BooksBook::wrap",
         skip_serializing_if = "Option::is_none"
     )]
     pub books: Option<Vec<Book>>,

packages/typespec-rust/test/spector/payload/xml/src/generated/models/xml_helpers.rs
@@ -11,24 +11,24 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename = "AllBooks")]
-pub(crate) struct BooksXmlBook {
+pub(crate) struct BooksBook {
     #[serde(default)]
     XmlBook: Option<Vec<Book>>,
 }
 
-impl BooksXmlBook {
+impl BooksBook {
     pub fn unwrap<'de, D>(deserializer: D) -> Result<Option<Vec<Book>>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        Ok(BooksXmlBook::deserialize(deserializer)?.XmlBook)
+        Ok(BooksBook::deserialize(deserializer)?.XmlBook)
     }
 
     pub fn wrap<S>(to_serialize: &Option<Vec<Book>>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        BooksXmlBook {
+        BooksBook {
             XmlBook: to_serialize.to_owned(),
         }
         .serialize(serializer)

packages/typespec-rust/test/tsp/MiscTests/main.tsp
@@ -1,9 +1,11 @@
 import "@azure-tools/typespec-client-generator-core";
 import "@azure-tools/typespec-azure-core";
 import "@typespec/http";
+import "@typespec/xml";
 
 using Azure.ClientGenerator.Core;
 using TypeSpec.Http;
+using TypeSpec.Xml;
 
 @service(#{
   title: "Various emitter tests",
@@ -253,3 +255,37 @@ model FoundWidgets {
 model Widget {
   name: string;
 }
+
+model ArrayOfTypeOne {
+  @Xml.name("Items")
+  items: TypeOne[];
+}
+
+model ArrayOfTypeTwo {
+  @Xml.name("Items")
+  items: TypeTwo[];
+}
+
+@Xml.name("Widget")
+model TypeOne {
+  name: string;
+}
+
+@Xml.name("Widget")
+model TypeTwo {
+  size: int32;
+}
+
+@get
+@route("/get-array-of-type-one")
+op getArrayOfTypeOne(): {
+  @header contentType: "application/xml";
+  @body body: ArrayOfTypeOne;
+};
+
+@get
+@route("/get-array-of-type-two")
+op getArrayOfTypeTwo(): {
+  @header contentType: "application/xml";
+  @body body: ArrayOfTypeTwo;
+};

packages/typespec-rust/test/tsp/PubCrate/main.tsp
@@ -4,6 +4,7 @@ import "@typespec/http";
 
 using Azure.ClientGenerator.Core;
 using Azure.Core;
+using TypeSpec.Http;
 
 @service(#{
   title: "All contents are pub(crate)",
@@ -24,4 +25,4 @@ model Widget {
 
 @access(Access.internal)
 @list
-op listWidgets(): Page<Widget>;
+op listWidgets(@query count?: int32): Page<Widget>;